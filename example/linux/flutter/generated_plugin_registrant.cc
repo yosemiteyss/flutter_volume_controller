3961a8e8a9013ac0193705f7e0227ca58c457bd3
yosemiteyss <kevinhonasdf@gmail.com>
2022-09-21 00:28:28 +0800

    init linux plugin

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_volume_controller/flutter_volume_controller_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_volume_controller_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterVolumeControllerPlugin");
+  flutter_volume_controller_plugin_register_with_registrar(flutter_volume_controller_registrar);
+}
