8c3b7cb28446e829b1cda3da6b601737d990fb70
yosemiteyss <kevinhonasdf@gmail.com>
2022-12-11 17:52:16 +0800

    fix floating point precision on Android

--- android/src/main/kotlin/com/yosemiteyss/flutter_volume_controller/VolumeBroadcastReceiver.kt
+++ android/src/main/kotlin/com/yosemiteyss/flutter_volume_controller/VolumeBroadcastReceiver.kt
@@ -5,9 +5,6 @@ import android.content.Context
 import android.content.Intent
 import io.flutter.plugin.common.EventChannel
 
-private const val EXTRA_VOLUME_STREAM_TYPE = "android.media.EXTRA_VOLUME_STREAM_TYPE"
-private const val VOLUME_CHANGED_ACTION = "android.media.VOLUME_CHANGED_ACTION"
-
 class VolumeBroadcastReceiver(
     private val event: EventChannel.EventSink?,
     private val audioStream: AudioStream,
@@ -16,7 +13,8 @@ class VolumeBroadcastReceiver(
     override fun onReceive(context: Context, intent: Intent?) {
         val streamType = intent?.extras?.getInt(EXTRA_VOLUME_STREAM_TYPE)
         if (intent?.action == VOLUME_CHANGED_ACTION && streamType == audioStream.streamType) {
-            event?.success(context.audioManager.getVolume(audioStream))
+            val volume = context.audioManager.getVolume(audioStream)
+            event?.success(volume.toString())
         }
     }
 }
