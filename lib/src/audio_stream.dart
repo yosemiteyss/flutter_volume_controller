8b346fd4e5273afc4b182a3e67671017106a4279
yosemiteyss <kevinhonasdf@gmail.com>
2023-01-19 23:34:00 +0800

    add extra android audio stream types

--- lib/src/audio_stream.dart
+++ lib/src/audio_stream.dart
@@ -1,1 +1,14 @@
-enum AudioStream { system, music }
+/// Android audio stream type.
+///
+/// [AudioStream.voiceCall] -> AudioManager.STREAM_VOICE_CALL
+/// [AudioStream.system] -> AudioManager.AudioManager.STREAM_SYSTEM
+/// [AudioStream.ring] -> AudioManager.STREAM_RING
+/// [AudioStream.music] -> AudioManager.STREAM_MUSIC
+/// [AudioStream.alarm] -> AudioManager.STREAM_ALARM
+enum AudioStream {
+  voiceCall,
+  system,
+  ring,
+  music,
+  alarm,
+}
